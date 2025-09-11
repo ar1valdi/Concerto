02a13952df9db6aa3e688de74cb0af326e5592b4
ar1valdi <janekkaczerski@gmail.com>
2025-09-11 18:26:16 +0200

    migrate db to net9

---
+++ src/Server/Migrations/20250911162429_PendingChanges.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Concerto.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_Workspaces_WorkspaceId",
+                table: "Post");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_Workspaces_WorkspaceId",
+                table: "Post",
+                column: "WorkspaceId",
+                principalTable: "Workspaces",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_Workspaces_WorkspaceId",
+                table: "Post");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_Workspaces_WorkspaceId",
+                table: "Post",
+                column: "WorkspaceId",
+                principalTable: "Workspaces",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
