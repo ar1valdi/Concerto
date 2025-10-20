316df4c0ea69c693e9a5fd2cd3f4402d731755aa
Montaso <28571739+Montaso@users.noreply.github.com>
2025-10-20 22:01:59 +0200

    Rewrite tree component to not use mud

---
+++ src/Client/UI/Components/Navigation/TreeItemData.cs
@@ -0,0 +1,14 @@
+﻿using Concerto.Shared.Models.Dto;
+
+namespace Concerto.Client.UI.Components.Navigation;
+
+public class TreeItemData
+{
+    public FolderContentItem? Item { get; set; }
+    public bool Expanded { get; set; }
+    public bool Expandable { get; set; }
+    public HashSet<TreeItemData> Children { get; set; } = new();
+    public long Id => Item?.Id ?? 0;
+    public string Name => Item?.Name ?? "";
+    public bool IsFolder => Item is FolderItem;
+}
